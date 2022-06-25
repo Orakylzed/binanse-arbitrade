9ebedb7ada6d505bbd82b0941a4ac7100a2861bb
Orakylzed <62772548+Orakylzed@users.noreply.github.com>
2022-06-25 15:36:16 +0300

    v0.2

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const rootElement = document.querySelector('#root');
 const refreshBtn = document.querySelector('#refresh');
 const btcPriceElem = document.querySelector('.btc-price-head');
 const statusElem = document.querySelector('.status');
+const allPairsElem = document.querySelector('#all-pairs');
+const sortElem = document.querySelector('#sort');
+const minProfitElem = document.querySelector('#min-profit');
+const clearBtn = document.querySelector('#clear');
 
 
 let DATA;
@@ -11,9 +15,17 @@ let curentPrices;
 let coinList = [];
 let coinPairObj = {};
 let arbitradeList = [];
+let minProfitValue = 0.01;
+
 
 refreshBtn.onclick = () => {
     sendRequest(requestURL).then( data => {DATA = data; dataHundler();} ).catch( (err) => console.log(err) );
+    rootElement.innerHTML = '<div class="loading"></div>';
+    checkMinProfit();
+}
+
+clearBtn.onclick = () => {
+    minProfitElem.value = '';
 }
 
 function sendRequest(url) {
@@ -33,7 +45,7 @@ function sendRequest(url) {
 }
 
 function dataAnalis(arr) {
-    curentPrices = [];
+        curentPrices = [];
     for (let i = 0; i < arr.length; i++ ) {
         curentPrices.push({name: arr[i].symbol, price: arr[i].askPrice});
     }
@@ -71,32 +83,45 @@ function getCoinPairObj() {
 
 function getArbitradeList() {
     arbitradeList = [];
+    
     for (let i = 0; i < coinList.length; i++) {
         if ((coinPairObj[`${coinList[i]}USDT`] != undefined) && (coinPairObj[`${coinList[i]}USDT`] != 0)) {
             if ((coinPairObj[`${coinList[i]}BTC`] != undefined) && (coinPairObj[`${coinList[i]}BTC`] != 0)) {
-                arbitradeList.push([coinList[i], coinPairObj[`${coinList[i]}USDT`], coinPairObj[`${coinList[i]}BTC`]]);
+                arbitradeList.push( [coinList[i], coinPairObj[`${coinList[i]}USDT`], coinPairObj[`${coinList[i]}BTC`]] );
             }
         }
+    }
+    for (let i = 0; i < arbitradeList.length; i++) {
+        let profit = calcProfit(arbitradeList[i][1], arbitradeList[i][2], btcUsdtPrice);
+        arbitradeList[i].push( profit );
+    }
+    if ( sortElem.checked ) {
+        arbitradeList.sort( (a, b) => a[3] > b[3] ? -1 : 1);
     }
 }
 
 function showPairs() {
     rootElement.innerHTML = '';
     for (let i = 0; i < arbitradeList.length; i++) {
-        let profit = calcProfit(arbitradeList[i][1], arbitradeList[i][2], btcUsdtPrice);
         let str = '';
         let elem = document.createElement('div');
         elem.classList.add('coin-price');
-        str += `<span class="coin-name">${ arbitradeList[i][0] }</span>: `;
-        str += `in USDT - <span class="usdt-price">${ arbitradeList[i][1] }</span>;`;
-        str += ` in BTC - <span class="btc-price">${ arbitradeList[i][2] }</span>`;
-        if (profit > 0) {
-            str += ` Profit: <span class="profit plus-profit">${ String(profit).slice(0, 4) }</span> %`;
-        } else {
-            str += ` Profit: <span class="profit minus-profit">${ String(profit).slice(0, 5) }</span> %`;
+        if ( arbitradeList[i][3] > minProfitValue ) {
+            str += `<span class="coin-name">${ arbitradeList[i][0] }</span>: `;
+            str += `in USDT - <span class="usdt-price">${ arbitradeList[i][1] }</span>;`;
+            str += ` in BTC - <span class="btc-price">${ arbitradeList[i][2] }</span>`;
+            str += ` Profit: <span class="profit plus-profit">${ String(arbitradeList[i][3]).slice(0, 4) }</span> %`;
+            elem.innerHTML = str;
+            rootElement.append(elem);
+        } else if ( allPairsElem.checked ) {
+            str += `<span class="coin-name">${ arbitradeList[i][0] }</span>: `;
+            str += `in USDT - <span class="usdt-price">${ arbitradeList[i][1] }</span>;`;
+            str += ` in BTC - <span class="btc-price">${ arbitradeList[i][2] }</span>`;
+            str += ` Profit: <span class="profit minus-profit">${ String(arbitradeList[i][3]).slice(0, 5) }</span> %`;
+            elem.innerHTML = str;
+            rootElement.append(elem);
         }
-        elem.innerHTML = str;
-        rootElement.append(elem);
+        
     }
     btcPriceElem.innerHTML = btcUsdtPrice.slice(0, 8);
     statusElem.classList.remove('hide');
@@ -111,6 +136,12 @@ function calcProfit(pr1, pr2, prbtc) {
     return profit;
 }
 
+function checkMinProfit() {
+    if ( Number(minProfitElem.value) != NaN ) {
+        minProfitValue = Number(minProfitElem.value);
+    }
+}
+
 function dataHundler() {
     dataAnalis(DATA);
     arrFilter();
@@ -120,5 +151,4 @@ function dataHundler() {
     showPairs();
 }
 
-sendRequest(requestURL).then( data => {DATA = data; dataHundler();} ).catch( (err) => console.log(err) );
 
